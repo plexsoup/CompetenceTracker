baf95953f3de2fcc9cdc89f530fb1a8d75840e0f
plexsoup <plexsoup@users.noreply.github.com>
2020-05-30 17:04:34 -0700

    initial push
    just design notes

---
+++ Mastery/DesignNotes.gd
@@ -0,0 +1,13 @@
+"""
+Competence Tracker is an app intended to keep track of subject matter mastery.
+
+It serves as a roadmap, to see what milestones we need to hit in order to achieve subject matter mastery.
+It also serves as a motivational tool, to encourage users to make those gains.
+
+Inspired by a simple steeplechase type boardgame, with runners jumping over obstacles on their way toward a finish line.
+
+This pedagogy has been widely adopted, including by the Pat and Linda Parelli.
+
+"""
+
+
